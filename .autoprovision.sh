23627797728d573d858ecaccba726d9e6ff8c7f6
carefreecaribou <55297934+carefreecaribou@users.noreply.github.com>
2024-04-06 14:18:23 -0400

    Adjust autorun

--- .autoprovision.sh
+++ .autoprovision.sh
@@ -20,5 +20,5 @@ cd ~/Development/Debain12PentestWorkstation
 # Run the Playbook
 ansible-galaxy install -r requirements.yml
 echo "remove_autostart: true" >> config.yml
-echo 'gnome-terminal -- bash -c "cd ~/Development/Debain12PentestWorkstation && ansible-playbook main.yml --ask-become-pass; bash"' >> ~/.profile
+echo 'gnome-terminal -- bash -c "cd ~/Development/Debain12PentestWorkstation && ansible-playbook main.yml --ask-become-pass; bash"' >> ~/.bashrc
 sudo reboot now
